835e73edbf6efc3511dd62ed88d5cf7478a7fa4f
이종경 <gyoung3063413@naver.com>
2024-03-26 19:13:00 +0900

    Chore: Prettier 설정 변경 및 react-router-dom 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -6,4 +8,10 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
